8e43e8ec0648514e266fe2875dc06c4dc9b80adb
MGS Creativa <martin@mgscreativa.com>
2023-08-09 17:09:02 -0300

    Add codePushOptions to main script

--- App.tsx
+++ App.tsx
@@ -2,10 +2,15 @@ import { registerRootComponent } from "expo";
 import { ExpoRoot } from "expo-router";
 import codePush from "react-native-code-push";
 
+let codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.IMMEDIATE,
+};
+
 // Must be exported or Fast Refresh won't update the context
 export function App() {
   const ctx = require.context("./app");
   return <ExpoRoot context={ctx} />;
 }
 
-registerRootComponent(codePush(App));
+registerRootComponent(codePush(codePushOptions)(App));
