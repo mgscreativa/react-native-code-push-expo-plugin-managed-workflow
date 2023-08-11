2d60aefcc6d8c945a0dd20a951c70e56507ecd47
MGS Creativa <martin@mgscreativa.com>
2023-08-11 11:36:46 -0300

    Revert "Revert "Conform App.tsx to expo-router/entry.js""
    This reverts commit 9bd68406628b25c41e9aa5e4672de61bed6c966b.

--- App.tsx
+++ App.tsx
@@ -1,26 +1,21 @@
-import { registerRootComponent } from "expo";
-import { ExpoRoot } from "expo-router";
-import Head from "expo-router/head";
+// `@expo/metro-runtime` MUST be the first import to ensure Fast Refresh works
+// on web.
+import "@expo/metro-runtime";
+
+// This file should only import and register the root. No components or exports
+// should be added here.
+import { renderRootComponent } from "expo-router/src/renderRootComponent";
+
+import { App } from "expo-router/_app";
 import codePush from "react-native-code-push";
+
+/**
+ * @todo: Monitor expo-router/entry.js for possible changes or updates!
+ */
 
 let codePushOptions = {
   checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
   installMode: codePush.InstallMode.IMMEDIATE,
 };
 
-const ctx = require.context(
-    process.env.EXPO_ROUTER_APP_ROOT!,
-    true,
-    /.*/
-);
-
-// Must be exported or Fast Refresh won't update the context
-export function App() {
-  return (
-      <Head.Provider>
-        <ExpoRoot context={ctx} />
-      </Head.Provider>
-  );
-}
-
-registerRootComponent(codePush(codePushOptions)(App));
+renderRootComponent(codePush(codePushOptions)(App));
